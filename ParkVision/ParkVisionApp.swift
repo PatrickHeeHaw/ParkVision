eae82a3cdc5c933c875c802cc74e7437e8a23321
PatrickHeeHaw <Patrick.ha09@gmail.com>
2025-11-01 13:07:39 -0500

    Initial Commit

---
+++ ParkVision/ParkVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkVisionApp.swift
+//  ParkVision
+//
+//  Created by Patrick Ha on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ParkVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParkVisionTests/ParkVisionTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParkVisionTests.swift
+//  ParkVisionTests
+//
+//  Created by Patrick Ha on 11/1/25.
+//
+
+import Testing
+@testable import ParkVision
+
+struct ParkVisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
